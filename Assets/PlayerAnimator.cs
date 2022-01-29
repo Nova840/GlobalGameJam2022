404fd95d8b03136df58502fad65651bf24c59ae5
Michael Cooper <MTCooper115@gmail.com>
2022-01-29 14:28:54 -0500

    basic weapon effects.

--- Assets/PlayerAnimator.cs
+++ Assets/PlayerAnimator.cs
@@ -2,6 +2,11 @@ using UnityEngine;
 
 public sealed class PlayerAnimator : MonoBehaviour
 {
+	[SerializeField]
+	private float speed = 1.5f;
+	[SerializeField]
+	private float distance = 0.025f;
+
 	private void Update()
-		=> transform.localPosition = new Vector2(0, Mathf.Sin(Time.time * 2) * 0.05f);
+		=> transform.localPosition = new Vector3(0, Mathf.Sin(Time.time * speed) * distance, transform.localPosition.z);
 }
