a1d30728efee727ef441c8f3a8894beec62c9d3f
Michael Cooper <MTCooper115@gmail.com>
2022-01-28 21:54:02 -0500

    Add Start Screen.

---
+++ Assets/Scripts/SceneHandler.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public sealed class SceneHandler : MonoBehaviour
+{
+	[SerializeField]
+	private string gameScene;
+	[SerializeField]
+	private string mainMenuScene;
+	[SerializeField]
+	private string instructionsScene;
+	[SerializeField]
+	private string creditsScene;
+	[SerializeField]
+	private string endScene;
+
+	public void StartGame() => SceneManager.LoadScene(gameScene, LoadSceneMode.Single);
+	public void GoToInstructions() => SceneManager.LoadScene(instructionsScene, LoadSceneMode.Single);
+	public void GoToCredits() => SceneManager.LoadScene(creditsScene, LoadSceneMode.Single);
+	public void GoToMainMenu() => SceneManager.LoadScene(mainMenuScene, LoadSceneMode.Single);
+	public void EndScreen() => SceneManager.LoadScene(endScene, LoadSceneMode.Single);
+	public void ExitGame() => Application.Quit();
+}
