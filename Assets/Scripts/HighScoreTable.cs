aaf1cb34ba04f36d57615266483bfba45e538e9c
Adam Zmudzinski <adamzmudzinski2@gmail.com>
2022-01-29 19:12:56 -0500

    Add high score submitting to end screen.

--- Assets/Scripts/HighScoreTable.cs
+++ Assets/Scripts/HighScoreTable.cs
@@ -4,26 +4,33 @@ using UnityEngine;
 using GameJolt.API;
 using GameJolt.API.Objects;
 using TMPro;
+using UnityEngine.UI;
 
 public class HighScoreTable : MonoBehaviour
 {
 
-    private static List<HighScoreTable> allHighScoreTables = new List<HighScoreTable>();
-
     [SerializeField]
     private GameObject gameJoltAPIPrefab;
+
+    [SerializeField]
+    private Button submitButton;
+
+    [SerializeField]
+    private TMP_InputField nameInputField;
+
+    [SerializeField]
+    private int maxCharsForName = 10;
 
     private static GameObject gameJoltAPI;
 
     [SerializeField]
     private RectTransform scoreTemplate;
 
+    private List<GameObject> scoreElements = new List<GameObject>();
+
     [Min(0)]
     [SerializeField]
     private float spacing = 20;
-
-    private void Awake() => allHighScoreTables.Add(this);
-    private void OnDestroy() => allHighScoreTables.Remove(this);
 
     private void Start()
     {
@@ -33,25 +40,51 @@ public class HighScoreTable : MonoBehaviour
         scoreTemplate.gameObject.SetActive(false);
     }
 
-    private static void GetScores() => Scores.Get(UpdateScores, 0, 10);
-    private static void UpdateScores(Score[] scores)
+    private void GetScores() => Scores.Get(UpdateScores, 0, 10);
+    private void UpdateScores(Score[] scores)
     {
-        foreach (HighScoreTable hst in HighScoreTable.allHighScoreTables)
+        foreach (GameObject se in scoreElements)
+            Destroy(se);
+        scoreElements.Clear();
+        for (int i = 0; i < scores.Length; i++)
         {
-            for (int i = 0; i < scores.Length; i++)
-            {
-                RectTransform rt = Instantiate(hst.scoreTemplate, hst.transform).GetComponent<RectTransform>();
-                rt.gameObject.SetActive(true);
-                rt.anchoredPosition = new Vector2(hst.scoreTemplate.anchoredPosition.x, hst.scoreTemplate.anchoredPosition.y + (hst.spacing * (-i - 1)));
-                rt.Find("Name Text").GetComponent<TMP_Text>().text = scores[i].PlayerName;
-                rt.Find("Number Text").GetComponent<TMP_Text>().text = scores[i].Text;
-            }
+            RectTransform rt = Instantiate(scoreTemplate, transform).GetComponent<RectTransform>();
+            rt.gameObject.SetActive(true);
+            rt.anchoredPosition = new Vector2(scoreTemplate.anchoredPosition.x, scoreTemplate.anchoredPosition.y + (spacing * -i));
+            rt.Find("Name Text").GetComponent<TMP_Text>().text = scores[i].PlayerName;
+            rt.Find("Number Text").GetComponent<TMP_Text>().text = scores[i].Text;
+            scoreElements.Add(rt.gameObject);
         }
     }
 
-    public static void AddScore(int score, string name)
+    public void AddScore(int score, string name)
     {
-        Scores.Add(score, score.ToString(), name, 0, "", (success) => { if (success) GetScores(); });
+        Scores.Add(score, score.ToString(), name, 0, "", AddScoreCallback);
+    }
+
+    private void AddScoreCallback(bool success)
+    {
+        if (success)
+        {
+            nameInputField.text = "";
+            nameInputField.interactable = false;
+            GetScores();
+        }
+        else
+        {
+            submitButton.interactable = true;
+        }
+    }
+
+    public void OnSubmitButtonClick()
+    {
+        AddScore(ScoreManager.LoadScore(), nameInputField.text);
+        submitButton.interactable = false;
+    }
+
+    public void OnNameInputFieldValueChanged()
+    {
+        nameInputField.text = nameInputField.text.Substring(0, Mathf.Clamp(nameInputField.text.Length, 0, maxCharsForName));
     }
 
 }
